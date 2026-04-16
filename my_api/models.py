9da85cf3b3dd19d36dc014aca90f94abbb4cb5ff
Elmurod <elmurodmarimov7655@gmail.com>
2026-04-16 16:34:12 +0500

    add book models, serializers, and views

--- my_api/models.py
+++ my_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
--- my_api/serializers.py
+++ my_api/serializers.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.serializers import HyperlinkedModelSerializer
-
+from .models import Book
 
 class UserSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = "__all__"
